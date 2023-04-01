be63cdb8eb254969c42991e9f410f82e844cbce8
Oleksandr Zvozdin <zvyozdin@gmail.com>
2023-04-01 18:36:24 +0200

    init commit

---
+++ src/test/java/com/example/resourceprocessor/ResourceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourceprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
